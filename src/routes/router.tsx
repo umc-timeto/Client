c2c5148c0bc97a39be0382eae9afe07c40fbda59
yeon-yeon1 <nojk0106@naver.com>
2026-02-17 01:49:41 +0900

    🐛 Fix: 리다이렉트 수정 (#17)
    Fix: 리다이렉트 수정 (#17)

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -24,7 +24,7 @@ const publicChildren: RouteObject[] = [
   { index: true, element: <LoginPage /> },
   { path: "login", element: <LoginPage /> },
 
-  { path: "/auth/kakao/callback", element: <KakaoCallbackPage /> },
+  { path: "auth/kakao/callback", element: <KakaoCallbackPage /> },
 ];
 
 const protectedChildren: RouteObject[] = [
@@ -51,6 +51,7 @@ const routes: RouteObject[] = [
         element: <ProtectedLayout />,
         children: protectedChildren,
       },
+      { path: "*", element: <Navigate to="/login" replace /> },
     ],
   },
 ];
